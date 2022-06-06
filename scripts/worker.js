940785e27f8c4e44c22ee00895c67ccb15777b14
Mathieu Poliquin <mathieu.poliquin@gmail.com>
2022-06-06 09:32:47 -0400

    Update worker.js

--- scripts/worker.js
+++ scripts/worker.js
@@ -3,13 +3,13 @@
 function MatMulTest() {
     importScripts("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs");
 
-    const matSize = 2 * 1024;
+    const matSize = 4 * 1024;
 
     //WriteOutput('worker...');
 
     const mat1 = tf.randomUniform([matSize, matSize], 1, 2, tf.float32);
     const mat2 = tf.randomUniform([matSize, matSize], 1, 2, tf.float32);
-    WriteOutput(`3 Created two [${matSize},${matSize}] matrices filled with ones <br>`);
+    //WriteOutput(`3 Created two [${matSize},${matSize}] matrices filled with ones <br>`);
 
 
     var t0 = performance.now();
@@ -17,16 +17,16 @@ function MatMulTest() {
     //const result =  tf.matMul(mat1, mat2).dataSync();
     var t1 = performance.now();
 
-    WriteOutput(`matmul KernelMs ${matmulTime.kernelMs} ms <br>`);
-    WriteOutput(`matmul WallMs ${matmulTime.wallMs} ms <br>`);
+    //WriteOutput(`matmul KernelMs ${matmulTime.kernelMs} ms <br>`);
+    //WriteOutput(`matmul WallMs ${matmulTime.wallMs} ms <br>`);
     
     var time = matmulTime.kernelMs / 1000;
     //var time = (t1 - t0) / 1000
 
     totalFlops = 2 * Math.pow(matSize,3);
     gflops = 1.0e-9 * totalFlops / time;
-    WriteOutput(gflops + " GFlops <br>");
-    WriteValue('tr-matmul', `${time.toFixed(3)} s  <br> ${gflops.toFixed(3)} GFlops/s`);
+    //WriteOutput(gflops + " GFlops <br>");
+    //WriteValue('tr-matmul', `${time.toFixed(3)} s  <br> ${gflops.toFixed(3)} GFlops/s`);
 
     mat1.dispose();
     mat2.dispose();
