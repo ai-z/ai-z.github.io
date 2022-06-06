2888621c82b00e906327ae76d3a00a064ce20e05
Mathieu Poliquin <mathieu.poliquin@gmail.com>
2022-06-06 18:49:08 -0400

    Update worker.js

--- scripts/worker.js
+++ scripts/worker.js
@@ -16,7 +16,8 @@ function MatMulTest() {
 
 
     var t0 = performance.now();
-    const matmulTime = await tf.time(() => tf.matMul(mat1, mat2));
+    const matmulTime = tf.time(() => tf.matMul(mat1, mat2));
+    matmulTime.dataSync();
     //const result =  tf.matMul(mat1, mat2).dataSync();
     var t1 = performance.now();
 
