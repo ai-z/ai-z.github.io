d09f70b560b93c363a3b504a7f1b03773eb318df
Mathieu Poliquin <mathieu.poliquin@gmail.com>
2022-06-07 11:10:50 -0400

    Update worker.js

--- scripts/worker.js
+++ scripts/worker.js
@@ -11,47 +11,53 @@ async function MatMulTest() {
     tf.setBackend('webgl');
 
     //WriteOutput('worker...');
+    var bestTime = Infinity;
 
-    const mat1 = tf.randomUniform([matSize, matSize], 1, 2, tf.float32);
-    const mat2 = tf.randomUniform([matSize, matSize], 1, 2, tf.float32);
-    //WriteOutput(`3 Created two [${matSize},${matSize}] matrices filled with ones <br>`);
+    for(int j=0; j < 20; j++) {
+        const mat1 = tf.randomUniform([matSize, matSize], 1, 2, tf.float32);
+        const mat2 = tf.randomUniform([matSize, matSize], 1, 2, tf.float32);
+        //WriteOutput(`3 Created two [${matSize},${matSize}] matrices filled with ones <br>`);
 
 
-    var t0 = performance.now();
+        var t0 = performance.now();
+        
+        const profile_info = await tf.profile(() => {
+        let res = tf.matMul(mat1, mat2);
+        res.dataSync();
+        res.dispose();
+        
+        });
     
-    const profile_info = await tf.profile(() => {
-    let res = tf.matMul(mat1, mat2);
-    res.dataSync();
-    
-    });
-    
-    //res.dataSync();
-    //const result =  tf.matMul(mat1, mat2).dataSync();
-    var t1 = performance.now();
+        //res.dataSync();
+        //const result =  tf.matMul(mat1, mat2).dataSync();
+        var t1 = performance.now();
 
-    console.log(profile_info);
+        console.log(profile_info);
 
-    //WriteOutput(`matmul KernelMs ${matmulTime.kernelMs} ms <br>`);
-    //WriteOutput(`matmul WallMs ${matmulTime.wallMs} ms <br>`);
-    let totalKernelMs = 0;
-    for (let i = 0; i < profile_info.kernels.length; i++) {
-        totalKernelMs += profile_info.kernels[i].kernelTimeMs;
+        //WriteOutput(`matmul KernelMs ${matmulTime.kernelMs} ms <br>`);
+        //WriteOutput(`matmul WallMs ${matmulTime.wallMs} ms <br>`);
+        let totalKernelMs = 0;
+        for (let j = 0; j < profile_info.kernels.length; j++) {
+            totalKernelMs += profile_info.kernels[j].kernelTimeMs;
+        }
+        
+        var time = (totalKernelMs) / 1000;
+
+        if (time < bestTime)
+            bestTime = time;
+    
+
+        mat1.dispose();
+        mat2.dispose();
+
     }
-    
-    var time = (totalKernelMs) / 1000;
-    //var time = (t1 - t0) / 1000
-
-    
 
     totalFlops = 2 * Math.pow(matSize,3);
-    gflops = 1.0e-9 * totalFlops / time;
+    gflops = 1.0e-9 * totalFlops / besttime;
     //WriteOutput(gflops + " GFlops <br>");
     //WriteValue('tr-matmul', `${time.toFixed(3)} s  <br> ${gflops.toFixed(3)} GFlops/s`);
 
     postMessage(gflops);
-
-    mat1.dispose();
-    mat2.dispose();
 }
 
 
