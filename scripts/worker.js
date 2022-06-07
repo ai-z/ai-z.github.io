6dfa98b3e09324058eb527bd02122d909cb569d2
Mathieu Poliquin <mathieu.poliquin@gmail.com>
2022-06-07 12:18:39 -0400

    Update worker.js

--- scripts/worker.js
+++ scripts/worker.js
@@ -6,7 +6,7 @@ async function MatMulTest() {
 
     importScripts("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js");
 
-    const matSize = 4 * 1024;
+    const matSize = 1 * 1024;
 
     await tf.setBackend('webgl');
 
